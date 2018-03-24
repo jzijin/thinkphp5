e5d84d01a9af958faa085c4478819ba4be8d7083
jzijin <jzijin@163.com>
2018-03-24 20:52:30 +0800

    添加del 方法

---
+++ thinkphp/runtime/temp/b3097f23a8b2fac57d3cc13aca6c2fcc.php
@@ -0,0 +1,13 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:65:"/var/www/html/tp5/public/../application/index/view/index/put.html";i:1521877493;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    hello world!!!!
+</body>
+</html>
